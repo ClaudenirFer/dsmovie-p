667a7be7b1715182208dc2a9617a28d7784c8222
ClaudenirFer <claudenir_fer@yahoo.com.br>
2022-09-10 17:12:24 -0300

    feat(frontend/Routes):   implementa Rotas em App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,22 @@
-import { ReactComponent as GithubIcon } from "./assets/github.svg";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar/Navbar";
+import Listing from 'pages/Listing/Listing';
+import Form from 'pages/Form/Form';
+
+
 
 function App() {
   return (
-    <Navbar />
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Listing />} />
+        <Route path="/form">
+          <Route path=":movieId" element={<Form />} />
+        </Route>
+      </Routes>
+    
+    </BrowserRouter>
   );
 }
 
