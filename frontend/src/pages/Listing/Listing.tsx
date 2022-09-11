ece8bbcb58719ceb249554fec7ac97c88b8dd7ae
ClaudenirFer <claudenir_fer@yahoo.com.br>
2022-09-10 23:53:18 -0300

    refactor (frontend / componente listing) : implementa o layout dos cards com grid e breakpoints do bootstrap

--- frontend/src/pages/Listing/Listing.tsx
+++ frontend/src/pages/Listing/Listing.tsx
@@ -1,11 +1,33 @@
-import Pagination from 'components/pagination/Pagination';
-import './listing.css'
+import MovieCard from "components/MovieCard/MovieCard";
+import Pagination from "components/Pagination/Pagination";
+import "./listing.css";
 
-function Listing () {
+function Listing() {
+  return (
+    <>
+      <Pagination />
 
-    return (
-        <Pagination />
-    )
+      <div className="container">
+        <div className="row">
+          <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+            <MovieCard />
+          </div>
+          <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+            <MovieCard />
+          </div>
+          <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+            <MovieCard />
+          </div>
+          <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+            <MovieCard />
+          </div>
+          <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+            <MovieCard />
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default Listing;
