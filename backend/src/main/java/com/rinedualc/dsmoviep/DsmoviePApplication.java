2c74e3478a6ab40b72b6a7133a980e88b338af4a
ClaudenirFer <claudenir_fer@yahoo.com.br>
2022-09-11 22:46:14 -0300

    feat(backend/Configuração e dependências) :  inicializa repositório backend com as dependências geradas no initializr e adiciona o plugin maven ao projeto

---
+++ backend/src/main/java/com/rinedualc/dsmoviep/DsmoviePApplication.java
@@ -0,0 +1,13 @@
+package com.rinedualc.dsmoviep;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DsmoviePApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DsmoviePApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/rinedualc/dsmoviep/DsmoviePApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rinedualc.dsmoviep;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsmoviePApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
